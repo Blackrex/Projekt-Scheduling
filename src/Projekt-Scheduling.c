9b96320c8ab910b90ec20274f38bcc3a80d72c47
Blackrex <thomas-schaffner@web.de>
2015-06-14 19:35:33 +0200

    Test

---
+++ src/Projekt-Scheduling.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Projekt-Scheduling.c
+ Author      : Thomas Schaffner; Marc Schlich; Mike Engel
+ Version     : 0.1
+ Copyright   : HRW
+ Description : Scheduling
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Prozess.h"
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ src/Prozess.h
@@ -0,0 +1,11 @@
+/*
+ * Prozess.h
+ *
+ *  Created on: 14.06.2015
+ *      Author: Scareface
+ */
+
+#ifndef PROZESS_H_
+#define PROZESS_H_
+
+#endif /* PROZESS_H_ */
